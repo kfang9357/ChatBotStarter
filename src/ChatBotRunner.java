b341b0acd9871688d6a3a34a45b3bdb3ca4c85e4
kfang9357 <43207499+kfang9357@users.noreply.github.com>
2018-11-08 19:57:16 -0500

    fixed runner, fixing chatbox soon

--- src/ChatBotRunner.java
+++ src/ChatBotRunner.java
@@ -19,7 +19,7 @@ public class ChatBotRunner
 		//int whichChatBot = 0;
 
 		Scanner in = new Scanner (System.in);
-		System.out.println("Welcome to WackDonalds, are you here for breakfast, lunch, or dinner?");
+		System.out.println("Welcome to would you like to order breakfast, lunch or dinner?");
 		String userInput = in.nextLine();
 		String statement = "";
 
@@ -27,22 +27,23 @@ public class ChatBotRunner
 		{
 			//Use Logic to control which chatbot is handling the conversation\
 			//This example has only chatbot1
-			if (userInput.equals("breakfast"))
+			if (userInput.equals("breakfast") || (userInput.equals("Breakfast")))
 			{
 				chatbot1.chatLoop(statement);
 				statement = in.nextLine();
 			}
-			else if(userInput.equals("lunch"))
+			if(userInput.equals("lunch") || (userInput.equals("Lunch")))
 			{
 				chatbot2.chatLoop(statement);
 				statement = in.nextLine();
 			}
-			else if(statement.indexOf("dinner") >= 0)
+			else if(userInput.equals("dinner") || (userInput.equals("Dinner")))
 			{
 				chatbot3.chatLoop(statement);
 				statement = in.nextLine();
 			}
 		}
+		System.out.println("Thank you for ordering from WcDonalds. We hope to see you again!");
 	}
 
 }
