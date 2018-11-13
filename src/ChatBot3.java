309ae8051a6c00b5e1ad2b8a08d038a5c34e447e
kfang9357 <43207499+kfang9357@users.noreply.github.com>
2018-11-13 06:46:09 -0500

    brushed up methods, polishing

--- src/ChatBot3.java
+++ src/ChatBot3.java
@@ -112,7 +112,7 @@ public class ChatBot3
 		return "The" + restOfStatement + " is " + getCompliment();
 	}
 
-	private String transformCanIGet(String statement)
+	private String transformCanIGetk(String statement)
 	{
 		//  Remove the final period, if there is one
 		statement = statement.trim();
